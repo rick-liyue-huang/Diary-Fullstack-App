b8554e7ff128c72464b30354b55d85640fc0aea3
Rick Huang <rick.liyue.huang@gmail.com>
2025-02-02 13:12:39 +1100

    ci: initiate the project

---
+++ Diary-App.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Diary_App.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
